d45af97d0f2780dbaae0668c4e9c7c567f87fd5b
hemantraghav-123 <hemant.23b0131263@abes.ac.in>
2025-01-14 10:30:30 +0530

    Add files via upload

---
+++ Date format.js
@@ -0,0 +1,76 @@
+// const d = new Date("2015-3")
+// console.log(d)
+
+// const n = new Date("2015-03-25T12:00:00z")
+// console.log(n)
+
+// const g = new Date("3/12/2015")
+// console.log(g)
+
+// const b = new Date("2015")
+// console.log(b)
+
+// const a = new Date("2015-03-25")
+// console.log(a)
+
+// const z = new Date("Mar 25 2015")
+// console.log(z)
+
+// const x = new Date("Mar 21, 2015")
+// console.log(x)
+
+
+// const d = new Date("Mar 21, 2015")
+// console.log(d.getMonth())
+
+// const x = new Date()
+// console.log(x.getMonth())
+
+// const a = new Date("2015-03-25")
+// console.log(a.getDate())
+
+// const b = new Date()
+// console.log(b.getDate())
+
+// const d= new Date()
+// console.log(d.getHours())
+
+// const a= new Date("2015-03-25")
+// console.log(a.getHours())
+
+// const d= new Date()
+// console.log(d.getMinutes())
+
+// const a= new Date("2015-03-25")
+// console.log(a.getHours())
+
+// const d= new Date()
+// console.log(d.getMilliseconds())
+
+// const a= new Date("2015-03-25")
+// console.log(a.getMilliseconds())
+
+
+// const a= new Date("2015-03-25")
+//  console.log(a.setMonth())
+
+//  const b= new Date()
+//  console.log(b.setMonth(12))
+
+//  const a= new Date("2015-03-25")
+//  console.log(a.setMonth(10))
+
+
+// let text ="";
+// const today= new Date();
+// const someday= new Date();
+// someday.setFullYear(2100,0,14);
+
+// if(someday>today){
+//     text ="today is before january 14,2100";
+// }else{
+//     text ="today is after january 14,2100";
+// }
+// console.log(text)
+
+

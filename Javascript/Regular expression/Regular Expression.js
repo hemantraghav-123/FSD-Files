0f214be325a312d7877728260566467ed439fa1e
hemantraghav-123 <hemant.23b0131263@abes.ac.in>
2025-02-01 15:41:48 +0530

    Add files via upload

---
+++ Javascript/Regular expression/Regular Expression.js
@@ -0,0 +1,29 @@
+// let text="Visit ABESEC!";
+// let n=text.search("ABESEC");
+// console.log(n)
+
+// let ntext="Visit Microsoft!";
+// let result=text.replace("Microsoft","ABESEC");
+// console.log(result)
+
+// const pattern=/e/;
+// test=pattern.test("The best things in life are free!")
+// console.log(test)
+
+// exe=pattern.exec("The best things in the life are free!")
+// console.log(exe)
+
+let text="Visit ABESEC!";
+let n=text.search("ABESEC");
+console.log(n)
+
+let ntext="Visit Microsoft!";
+let result=text.replace("Microsoft","ABESEC");
+console.log(result)
+
+const pattern=/e/;
+test=pattern.test("The best things in life are free!")
+console.log(test)
+
+exe=pattern.exec("The best things in the life are free!")
+console.log(exe)
